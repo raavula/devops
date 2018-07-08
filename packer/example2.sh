8b05995e6bdb01621d2b79089e6261caac1ff043
avavula <avavula@cisco.com>
2018-07-07 23:26:41 -0700

    packer example

---
+++ packer/example2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#if you wanted to use a source_ami instead of a source_ami_filter it might look something like this: "source_ami": "ami-fce3c696",
+
+export AWS_ACCESS_KEY_ID=MYACCESSKEYID
+export AWS_SECRET_ACCESS_KEY=MYSECRETACCESSKEY
+packer build example2.json
