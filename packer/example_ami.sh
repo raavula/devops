92353270106f4d6d8648a6cc36fd8284d27eabee
avavula <avavula@cisco.com>
2018-09-15 23:50:42 -0700

    apikey removed

--- packer/example_ami.sh
+++ packer/example_ami.sh
@@ -2,9 +2,7 @@
 
 #if you wanted to use a source_ami instead of a source_ami_filter it might look something like this: "source_ami": "ami-fce3c696",
 
-export AWS_ACCESS_KEY_ID=AKIAJHQAUFTP5WQOPSPA
-export AWS_SECRET_ACCESS_KEY=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
-#export AWS_ACCESS_KEY_ID=MYACCESSKEYID
-#export AWS_SECRET_ACCESS_KEY=MYSECRETACCESSKEY
+export AWS_ACCESS_KEY_ID=MYACCESSKEYID
+export AWS_SECRET_ACCESS_KEY=MYSECRETACCESSKEY
 export PATH=$PATH:/usr/local/bin/packer
 packer build example_ami.json
