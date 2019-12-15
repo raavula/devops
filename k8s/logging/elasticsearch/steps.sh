7c2da9b2f894ee32fc1b552361f8fa9143721b99
avavula <avavula@cisco.com>
2019-12-14 23:53:58 -0800

    logging eith elk example

--- k8s/logging/elasticsearch/steps.sh
+++ k8s/logging/elasticsearch/steps.sh
@@ -10,3 +10,7 @@ kubectl get service -n logging
 kubectl create -f kibana.yaml -n logging
 kubectl get pods -n logging
 kubectl get service -n logging
+
+
+kubectl proxy 
+http://localhost:8001/api/v1/namespaces/logging/services/kibana:/proxy/app/kibana
